1f7e81061233769a83627ddf01d67e0dcc6ba37f
jhl9412 <102011846+jhl9412@users.noreply.github.com>
2022-07-30 12:47:57 +0900

    update : servo, dc motor

---
+++ atmega/GccApplication6/servo.h
@@ -0,0 +1,20 @@
+﻿/*
+ * servo.h
+ *
+ * Created: 2022-07-29 오전 11:01:14
+ *  Author: KCCI03
+ */ 
+
+
+#ifndef SERVO_H_
+#define SERVO_H_
+#define F_CPU 14745600L
+#include <avr/interrupt.h>
+#include <util/delay.h>
+void servo_init(void);
+void servo(void);
+void set_speed(void);
+void dc_run();
+/*ISR(TIMER2_COMP_vect);*/
+
+#endif /* SERVO_H_ */
---
+++ atmega/GccApplication6/uart.h
@@ -0,0 +1,24 @@
+﻿/*
+ * uart.h
+ *
+ * Created: 2022-07-29 오전 10:46:34
+ *  Author: KCCI03
+ */ 
+
+#ifndef UART_H_
+#define UART_H_
+#define F_CPU 14745600L
+#include <avr/io.h>
+#include <util/delay.h>
+#include <string.h>
+
+void uart_init(void);
+void UART0_transmit(char data);
+uint8_t UART0_receive(void);
+void UART1_transmit(char data);
+uint8_t UART1_receive(void);
+void Trs(char* data);
+void get_msg(char *msg);
+int parsing(char *msg);
+
+#endif /* UART_H_ */
